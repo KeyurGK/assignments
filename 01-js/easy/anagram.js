996940d33135be5d59ceeddf772dce08da38802b
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2024-01-08 20:38:24 +0530

    solution for anagrams

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,28 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length != str2.length) {
+    return false;
+  }
+  let cleanStr1 = str1.replace(/[^\w]/g, "").toLowerCase();
+  let cleanStr2 = str2.replace(/[^\w]/g, "").toLowerCase();
 
+  const len1 = cleanStr1.length;
+  const len2 = cleanStr2.length;
+
+  if (len1 != len2) {
+    return false;
+  }
+
+  cleanStr1 = cleanStr1.split("").sort().join("");
+  cleanStr2 = cleanStr2.split("").sort().join("");
+
+  for (let i = 0; i < len1; i++) {
+    if (cleanStr1[i] != cleanStr2[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = isAnagram;
