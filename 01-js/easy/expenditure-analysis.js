f9abc96f79eb9a4c21a1b6bce43f97a68bfc6d82
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2024-01-29 20:58:14 +0530

    commiting counter solution

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -12,9 +12,46 @@
 	}
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
-
+const transactions = [
+  {
+    id: 1,
+    timestamp: 165607680000,
+    price: 10,
+    category: "Food",
+    itemName: "Pizza",
+  },
+  {
+    id: 2,
+    timestamp: 165607680010,
+    price: 12,
+    category: "Clothes",
+    itemName: "Pant",
+  },
+  {
+    id: 3,
+    timestamp: 1656076803000,
+    price: 20,
+    category: "Food",
+    itemName: "zza",
+  },
+];
 function calculateTotalSpentByCategory(transactions) {
-  return [];
-}
+  const categoryTotal = transactions.reduce(function (accumulator, item) {
+    const { category, price } = item;
+    accumulator[category] = (accumulator[category] || 0) + price;
+    return accumulator;
+  }, {});
+  console.log(categoryTotal);
+  const newArrayEntries = Object.entries(categoryTotal);
+  console.log(newArrayEntries);
+  const newArray = newArrayEntries.map(function ([category, price]) {
+    return {
+      category,
+      totalSpent: price,
+    };
+  });
 
-module.exports = calculateTotalSpentByCategory;
+  console.log(newArray);
+}
+calculateTotalSpentByCategory(transactions);
+// module.exports = calculateTotalSpentByCategory;
---
+++ week-2/01-async-js/easy/counter1_solution.js
@@ -0,0 +1,7 @@
+let a = 0;
+function counter() {
+  a = a + 1;
+  console.log(a);
+}
+
+setInterval(counter, 1000);
