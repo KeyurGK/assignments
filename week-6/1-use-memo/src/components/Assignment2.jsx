c8a35743f54442c2f58de1f892a6573bdfa9a298
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2024-03-21 10:41:35 +0530

    submitting solutions of useMemo

--- week-6/1-use-memo/src/components/Assignment2.jsx
+++ week-6/1-use-memo/src/components/Assignment2.jsx
@@ -1,33 +1,49 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 
-// In this assignment, you will create a component that renders a large list of sentences and includes an input field for filtering these items. 
+// In this assignment, you will create a component that renders a large list of sentences and includes an input field for filtering these items.
 // The goal is to use useMemo to optimize the filtering process, ensuring the list is only re-calculated when necessary (e.g., when the filter criteria changes).
 // You will learn something new here, specifically how you have to pass more than one value in the dependency array
 
-const words = ["hi", "my", "name", "is", "for", "to", "random", "word" ];
-const TOTAL_LINES = 1000;
+const words = ["hi", "my", "name", "is", "for", "to", "random", "word"];
+const TOTAL_LINES = 5;
 const ALL_WORDS = [];
 for (let i = 0; i < TOTAL_LINES; i++) {
-    let sentence = "";
-    for (let j = 0; j < words.length; j++) {
-        sentence += (words[Math.floor(words.length * Math.random())])
-        sentence += " "
-    }
-    ALL_WORDS.push(sentence);
+  let sentence = "";
+  for (let j = 0; j < words.length; j++) {
+    sentence += words[Math.floor(words.length * Math.random())];
+    sentence += " ";
+  }
+  ALL_WORDS.push(sentence);
 }
 
 export function Assignment2() {
-    const [sentences, setSentences] = useState(ALL_WORDS);
-    const [filter, setFilter] = useState("");
+  const [count, setCount] = useState(0);
+  const [sentences, setSentences] = useState(ALL_WORDS);
+  const [filter, setFilter] = useState("");
+  function counter() {
+    setCount(count + 1);
+  }
+  console.log("hi brfore");
+  const filteredSentences = useMemo(
+    function () {
+      return sentences.filter((x) => x.includes(filter));
+    },
+    [sentences, filter]
+  );
 
-    const filteredSentences = sentences.filter(x => x.includes(filter))
-
-    return <div>
-        <input type="text" onChange={(e) => {
-            setFilter(e.target.value)
-        }}></input>
-        {filteredSentences.map(word => <div>
-            {word}    
-        </div>)}
+  console.log("hi after");
+  return (
+    <div>
+      <button onClick={counter}>count ++ : {count}</button>
+      <input
+        type="text"
+        onChange={(e) => {
+          setFilter(e.target.value);
+        }}
+      ></input>
+      {filteredSentences.map((word) => (
+        <div>{word}</div>
+      ))}
     </div>
+  );
 }
--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -1,27 +1,48 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo } from "react";
 // You have been given a list of items you shopped from the grocery store
 // You need to calculate the total amount of money you spent
 
 export const Assignment3 = () => {
-    const [items, setItems] = useState([
-        { name: 'Chocolates', value: 10 },
-        { name: 'Chips', value: 20 },
-        { name: 'Onion', value: 30 },
-        { name: 'Tomato', value: 30 },
-        // Add more items as needed
-    ]);
+  const [items, setItems] = useState([
+    { name: "Chocolates", value: 10 },
+    { name: "Chips", value: 20 },
+    { name: "Onion", value: 30 },
+    { name: "Tomato", value: 30 },
+    // Add more items as needed
+  ]);
+  const [count, setCount] = useState(0);
+  // Your code starts here
+  console.log("hi before");
+  const totalValue = useMemo(
+    function () {
+      let totalValue = 0;
+      items.forEach(function (item) {
+        totalValue = totalValue + item.value;
+      });
+      return totalValue;
+    },
+    [items]
+  );
 
-    // Your code starts here
-    const totalValue = 0;
-    // Your code ends here
-    return (
-        <div>
-            <ul>
-                {items.map((item, index) => (
-                    <li key={index}>{item.name} - Price: ${item.value}</li>
-                ))}
-            </ul>
-            <p>Total Value: {totalValue}</p>
-        </div>
-    );
+  console.log("hi after");
+  // Your code ends here
+  return (
+    <div>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>
+            {item.name} - Price: ${item.value}
+          </li>
+        ))}
+      </ul>
+      <p>Total Value: {totalValue}</p>
+      <button
+        onClick={function () {
+          setCount(count + 1);
+        }}
+      >
+        Counter {count}
+      </button>
+    </div>
+  );
 };
