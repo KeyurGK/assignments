47e472f609d491b27d7f6eb35e8d9922fdd6c529
KeyurGK <56074740+KeyurGK@users.noreply.github.com>
2024-03-24 15:46:03 +0530

    completed use-ref

--- week-6/3-use-ref/src/components/Assignment1.jsx
+++ week-6/3-use-ref/src/components/Assignment1.jsx
@@ -1,21 +1,47 @@
-import { useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 
 // Create a component with a text input field and a button. When the component mounts or the button is clicked, automatically focus the text input field using useRef.
 
+// export function Assignment1() {
+//   const inputRef = useRef(null);
+//   useEffect(() => {
+//     inputRef.current.focus();
+//   }, []);
+
+//   const handleButtonClick = () => {
+//     inputRef.current.focus();
+//   };
+
+//   return (
+//     <div>
+//       <input type="text" placeholder="Enter text here" ref={inputRef} />
+//       <button onClick={handleButtonClick}>Focus Input</button>
+//     </div>
+//   );
+// }
+
 export function Assignment1() {
+  const [inputValue, setInputValue] = useState("");
+  const previousInputValue = useRef("");
+  const [count, setCount] = useState(0);
 
-    useEffect(() => {
+  useEffect(() => {
+    previousInputValue.current = inputValue;
+    console.log("hi");
+  }, [inputValue]);
+  useEffect(() => {
+    console.log("hi2");
+  });
 
-    }, []);
-
-    const handleButtonClick = () => {
-
-    };
-
-    return (
-        <div>
-            <input type="text" placeholder="Enter text here" />
-            <button onClick={handleButtonClick}>Focus Input</button>
-        </div>
-    );
-};
+  return (
+    <>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <h2>Current Value: </h2>
+      <h2>Previous Value: {previousInputValue.current}</h2>
+    </>
+  );
+}
--- week-6/3-use-ref/src/components/Assignment2.jsx
+++ week-6/3-use-ref/src/components/Assignment2.jsx
@@ -1,19 +1,48 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useRef } from "react";
 
 // Create a component that tracks and displays the number of times it has been rendered. Use useRef to create a variable that persists across renders without causing additional renders when it changes.
 
 export function Assignment2() {
-    const [, forceRender] = useState(0);
+  //   const [forceRender, setForceRender] = useState(0);
+  //   const renderCount = useRef(0);
+  //   const handleReRender = () => {
+  //     // Update state to force re-render
+  //     setForceRender(forceRender + 1);
+  //   };
+  //   const handleRefChange = () => {
+  //     console.log(renderCount);
+  //     renderCount.current = renderCount.current + 1;
+  //     console.log(renderCount);
+  //   };
 
-    const handleReRender = () => {
-        // Update state to force re-render
-        forceRender(Math.random());
-    };
+  //   return (
+  //     <div>
+  //       <p>This component has rendered {forceRender} times.</p>
+  //       <button onClick={handleReRender}>Force Re-render</button>
+  //       <button onClick={handleRefChange}>Ref Change</button>
+  //       <p>{renderCount.current}</p>
+  //     </div>
+  //     );
 
-    return (
-        <div>
-            <p>This component has rendered {0} times.</p>
-            <button onClick={handleReRender}>Force Re-render</button>
-        </div>
-    );
-};
+  const [count, setCount] = useState(0);
+
+  const numberOfTimesReRendered = useRef(0);
+  console.log(count);
+  const handleReRender = () => {
+    // Update state to force re-render
+    setCount(count + 1);
+  };
+  console.log(count);
+
+  console.log(numberOfTimesReRendered);
+  numberOfTimesReRendered.current = numberOfTimesReRendered.current + 1;
+
+  return (
+    <div>
+      <p>
+        This component has rendered {numberOfTimesReRendered.current} times.
+      </p>
+      <button onClick={handleReRender}>Force Re-render</button>
+    </div>
+  );
+}
